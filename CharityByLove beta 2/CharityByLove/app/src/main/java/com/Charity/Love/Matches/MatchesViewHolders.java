e1b631ec15a9ac9fc8720211987292558521897a
RobinGranberg <57355443+RobinGranberg@users.noreply.github.com>
2020-05-24 22:23:13 +0200

    Add files via upload

---
+++ CharityByLove beta 2/CharityByLove/app/src/main/java/com/Charity/Love/Matches/MatchesViewHolders.java
@@ -0,0 +1,36 @@
+package com.Charity.Love.Matches;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.Charity.Love.Chat.ChatActivity;
+import com.Charity.Love.R;
+//återkom till denna, vet ej var den ligger ?
+
+
+public class MatchesViewHolders extends RecyclerView.ViewHolder implements View.OnClickListener{
+    public TextView mMatchId, mMatchName;
+    public ImageView mMatchImage;
+    public MatchesViewHolders(View itemView) {
+        super(itemView);
+        itemView.setOnClickListener(this);
+
+        mMatchId = (TextView) itemView.findViewById(R.id.Matchid);
+        mMatchName = (TextView) itemView.findViewById(R.id.MatchName);
+
+        mMatchImage = (ImageView) itemView.findViewById(R.id.MatchImage);
+    }
+
+    @Override
+    public void onClick(View view) {
+        Intent intent = new Intent(view.getContext(), ChatActivity.class);
+        Bundle b = new Bundle();
+        b.putString("matchId", mMatchId.getText().toString());
+        intent.putExtras(b);
+        view.getContext().startActivity(intent);
+    }
+}
